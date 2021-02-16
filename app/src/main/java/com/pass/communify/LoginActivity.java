ba53e90f2ed6e01e08ad9003187d237881479309
Xavier <Xavier.Robles.R@gmail.com>
2021-02-16 19:48:27 +0100

    Creacion De login de Google

--- app/src/main/java/com/pass/communify/LoginActivity.java
+++ app/src/main/java/com/pass/communify/LoginActivity.java
@@ -1,22 +1,54 @@
 package com.pass.communify;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity {
+//  AIzaSyDF1hxvPNuqu9aPVBh6CpXgjyoEcND3nqo
+public class LoginActivity extends AppCompatActivity implements
+        View.OnClickListener {
     MaterialButton buttonLog;
     TextView textR;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private TextView mStatusTextView;
+    private Button sign;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // Views
+        mStatusTextView = findViewById(R.id.status);
 
         buttonLog = (MaterialButton) findViewById(R.id.buttonLog);
         buttonLog.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +56,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, ElectionActivity.class);
                 startActivity(intent);
+
             }
         });
         textR = (TextView) findViewById(R.id.textViewR);
@@ -34,5 +67,142 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        //Configuracion de login con Google.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Cliente
+        // Cree un GoogleSignInClient con las opciones especificadas por gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        setContentView(R.layout.activity_login);
+        // Button listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.disconnect_button).setOnClickListener(this);
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setColorScheme(SignInButton.COLOR_LIGHT);
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // INICIO on_start_sign_in
+        // Compruebe la cuenta de inicio de sesión de Google existente, si el usuario ya ha iniciado sesión
+        // GoogleSignInAccount no será nulo.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        // END on_start_sign_in
+    }
+
+
+    // INICIO en Resultado de actividad
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Resultado devuelto al iniciar Intent desde GoogleSignInClient.getSignInIntent (...);
+        if (requestCode == RC_SIGN_IN) {
+            // La tarea devuelta de esta llamada siempre se completa, no es necesario adjuntar
+            // un oyente
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    //FIN del resultado de la actividad
+
+
+    //INICIO manejar Inicio sesión del Resultado
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Se registró correctamente, muestra la interfaz de usuario autenticada.
+            updateUI(account);
+        } catch (ApiException e) {
+            // El código de estado de ApiException indica el motivo detallado del error.
+            // Consulte la referencia de la clase GoogleSignInStatusCodes para obtener más información.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+    // Final
+
+
+    // Inico signIn
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // Final signIn
+
+    // Inicio signOut
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Inicio Excluido
+                        updateUI(null);
+                        // Fin Excluido
+                    }
+                });
+    }
+
+// Final signOut
+
+    // Inicio acceso revocado
+    private void revokeAccess() {
+        mGoogleSignInClient.revokeAccess()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Inicio Excluido
+                        updateUI(null);
+                        // Fin Excluido
+                    }
+                });
+    }
+    // Fin acceso revocado
+
+
+    private void updateUI(@Nullable GoogleSignInAccount account) {
+        if (account != null) {
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, account.getDisplayName()));
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+
+    }
+
+public void onClick(View v) {
+    switch (v.getId()) {
+        case R.id.sign_in_button:
+            signIn();
+            break;
+        case R.id.sign_out_button:
+            signOut();
+            break;
+        case R.id.disconnect_button:
+            revokeAccess();
+            break;
+        case R.id.buttonLog:
+            revokeAccess();
+            break;
+        case R.id.textViewR:
+            revokeAccess();
+            break;
+    }}
 }
