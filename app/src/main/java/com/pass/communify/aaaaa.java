47a02ef57c318f59eb4e4ce6853996d04b860c83
Jorge Ballesteros <jorgitoballes@gmail.com>
2021-02-16 23:35:09 +0100

    Nada demasiado importante

--- app/src/main/java/com/pass/communify/aaaaa.java
+++ app/src/main/java/com/pass/communify/aaaaa.java
@@ -1,21 +1,20 @@
 package com.pass.communify;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 import com.pass.communify.ui.main.SectionsPagerAdapter;
 
 public class aaaaa extends AppCompatActivity {
+
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +25,18 @@ public class aaaaa extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        ImageView iw = null;
+        iw = findViewById(R.id.fotoMecanica);
+
+        iw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = new Toast(context);
+                t.setText("sdfsdf");
+                t.show();
+            }
+        });
 
     }
 }
