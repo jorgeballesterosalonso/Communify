26f50a46f3785593d1d98c85e6a3fccd300d8b71
Jorge Ballesteros <jorgitoballes@gmail.com>
2021-02-17 20:27:05 +0100

    Comienzo de la pantalla de añadir producto

---
+++ app/src/main/java/com/pass/communify/AniadirProducto.java
@@ -0,0 +1,14 @@
+package com.pass.communify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AniadirProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aniadir_producto);
+    }
+}
