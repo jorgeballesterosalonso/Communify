e38867bdd651d7de3fd30b24e7afe12f6fbf10f3
Jorge Ballesteros <jorgitoballes@gmail.com>
2021-03-03 01:22:55 +0100

    Se puede añadir la foto a las características del producto compartido

--- app/src/main/java/com/pass/communify/AniadirProducto.java
+++ app/src/main/java/com/pass/communify/AniadirProducto.java
@@ -1,14 +1,52 @@
 package com.pass.communify;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AniadirProducto extends AppCompatActivity {
+    private ImageView imagen;
+    private Context context;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aniadir_producto);
+        imagen = findViewById(R.id.imagenProducto);
+        imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent galleryIntent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, 0);
+
+
+            }
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (null != data) {
+            imageUri = data.getData();
+            Log.d("URI", imageUri.toString());
+            imagen.setImageURI(imageUri);
+            //Do whatever that you desire here. or leave this blank
+
+        }
+
+    }
+
 }
