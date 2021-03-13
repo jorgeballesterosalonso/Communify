aca67eb98d2c7f9b317826bc762d6e5a1625d89d
34607 <mforal77@gmail.com>
2021-03-13 20:11:33 +0100

    3-Diseño LayoutConfig

---
+++ app/src/main/java/com/pass/communify/ConfiguracionActivity.java
@@ -0,0 +1,17 @@
+package com.pass.communify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+
+public class ConfiguracionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion);
+
+    }
+}
