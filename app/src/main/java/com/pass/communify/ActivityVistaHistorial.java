6c8ab2597e352e3988906391758c9cb40e99f7ba
Albersss <alber.garcia.lopez106@gmail.com>
2021-03-17 01:22:48 +0100

    añado recycler corregido

--- app/src/main/java/com/pass/communify/ActivityVistaHistorial.java
+++ app/src/main/java/com/pass/communify/ActivityVistaHistorial.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,13 +20,14 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class ActivityVistaHistorial extends AppCompatActivity {
-//    ArrayList<Historial> listaHistorial=new ArrayList<>();
+    //    ArrayList<Historial> listaHistorial=new ArrayList<>();
     RecyclerView rv;
     RecyclerView.LayoutManager lm;
     RecyclerView.Adapter ad;
-    String[] ListaProductos = {"Taladradora", "Termomix","Cortacesped", "Salir por Vallecas", "Pinzas de coche"};
+    String[] ListaProductos = {"Taladradora", "Termomix", "Cortacesped", "Salir por Vallecas", "Pinzas de coche"};
     String[] ListaPeticion = {"Solicitado", "Solicitado", "Prestado", "Solicitado", "Prestado"};
-    int[] listFotos = {R.drawable.fotobricolaje, R.drawable.fondococina, R.drawable.fotojardin,
+    Double[] puntuacion = {3.5, 1.5, 2.2, 4.5, 5.0};
+    int[] listaFotos = {R.drawable.fotobricolaje, R.drawable.fondococina, R.drawable.fotojardin,
             R.drawable.fotofiesta, R.drawable.fotomecanica};
 
 
@@ -33,53 +36,11 @@ public class ActivityVistaHistorial extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mostrar_activity);
 
-//        Historial h1 = new Historial();
-//        Historial h2 = new Historial();
-//        Historial h3 = new Historial();
-//        Historial h4 = new Historial();
-//        Historial h5 = new Historial();
-//        Historial h6 = new Historial();
-//
-//        //True, solicitado. False, enviado
-//        h1.setSolicitadoenviado("Solicitado");
-//        h1.setProducto("Taladradora eléctrica");
-//        h1.setFoto(R.drawable.fotobricolaje);
-//        h1.setCalificacion((float) 4.5);
-//        h2.setSolicitadoenviado("Solicitado");
-//        h2.setProducto("Termomix");
-//        h2.setFoto(R.drawable.fondococina);
-//        h2.setCalificacion((float) 3.5);
-//        h3.setSolicitadoenviado("Enviado");
-//        h3.setProducto("Cafetera Cetotec");
-//        h3.setFoto(R.drawable.fondococina);
-//        h3.setCalificacion((float) 5.0);
-//        h4.setSolicitadoenviado("Enviado");
-//        h4.setProducto("Cortacesped manual");
-//        h4.setFoto(R.drawable.fotojardin);
-//        h4.setCalificacion((float) 4.0);
-//        h5.setSolicitadoenviado("Solicitado");
-//        h5.setProducto("Salir por el barrio");
-//        h5.setFoto(R.drawable.fotofiesta);
-//        h5.setCalificacion((float) 2.5);
-//        h6.setSolicitadoenviado("Enviado");
-//        h6.setProducto("Maletín de arrancado");
-//        h6.setFoto(R.drawable.fotomecanica);
-//        h6.setCalificacion((float) 5.0);
-//
-//        listaHistorial = new ArrayList<>();
-//
-//        listaHistorial.add(h1);
-//        listaHistorial.add(h2);
-//        listaHistorial.add(h3);
-//        listaHistorial.add(h4);
-//        listaHistorial.add(h5);
-//        listaHistorial.add(h6);
-
         rv = findViewById(R.id.recycler);
         rv.setHasFixedSize(true);
         lm = new LinearLayoutManager(this);
         rv.setLayoutManager(lm);
-        MiAdaptador adaptador = new MiAdaptador(this, ListaProductos, ListaPeticion, listFotos);
+        MiAdaptador adaptador = new MiAdaptador(this, ListaProductos, ListaPeticion, puntuacion, listaFotos);
         rv.setAdapter(adaptador);
     }
 }
--- app/src/main/java/com/pass/communify/MiAdaptador.java
+++ app/src/main/java/com/pass/communify/MiAdaptador.java
@@ -11,14 +11,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.MiAdaptadorVistas> {
 
     Context context;
-    String[] productos, solicitados;
+    String[] productos;
+    String[] solicitados;
+    Double[] puntuacion;
     int[] fotos;
 
     public MiAdaptador(ArrayList<Historial> listaHistorial) {
@@ -26,24 +30,31 @@ public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.MiAdaptadorVis
 
 
     public class MiAdaptadorVistas extends RecyclerView.ViewHolder {
+        MiAdaptadorVistas contexto2 = this;
         TextView producto;
         TextView solicitado;
         ImageView rowImage;
         RatingBar estrellitas;
 
+
+
         public MiAdaptadorVistas(@NonNull View itemView) {
             super(itemView);
+
             producto = itemView.findViewById(R.id.tv_producto);
             solicitado = itemView.findViewById(R.id.tv_solicitado);
             rowImage = itemView.findViewById(R.id.iv_foto);
             estrellitas = itemView.findViewById(R.id.ratingBar);
-        }
-    }
 
-    public MiAdaptador(Context context, String[] productos, String[] solicitados, int[] fotos) {
+            }
+        }
+
+
+    public MiAdaptador(Context context, String[] productos, String[] solicitados, Double[] puntuacion, int[] fotos) {
         this.context = context;
         this.productos = productos;
         this.solicitados = solicitados;
+        this.puntuacion = puntuacion;
         this.fotos = fotos;
 
     }
@@ -59,9 +70,17 @@ public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.MiAdaptadorVis
 
     @Override
     public void onBindViewHolder(@NonNull MiAdaptador.MiAdaptadorVistas holder, int position) {
+        int idfoto = fotos[position];
         holder.producto.setText(productos[position]);
         holder.solicitado.setText(solicitados[position]);
-        holder.rowImage.setImageResource(fotos[position]);
+        holder.estrellitas.setRating(puntuacion[position].floatValue());
+        //holder.rowImage.setImageResource(fotos[position]);
+        Glide.with(context)
+                .load(idfoto)
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .circleCrop()
+                .into(holder.rowImage);
     }
 
     @Override
